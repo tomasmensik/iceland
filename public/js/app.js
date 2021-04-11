02987a8f261b778fc7cf4513331e2ae0d13af0bb
Tomáš Velička <tomasmensik2003@seznam.cz>
2021-04-11 17:04:35 +0200

    vymenaVlockyZaStrom+htmlCss

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@ let container;
 let camera;
 let renderer;
 let scene;
-let crystal;
+let tree;
 
 function init(){
     container = document.querySelector('.scene');
@@ -15,18 +15,18 @@ function init(){
     const fov = 35;
     const aspect = container.clientWidth / container.clientHeight;
     const near = 0.1;
-    const far = 500;
+    const far = 10000;
 
     //Camera setup
     camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.set(0, 0, 10);
-
+    camera.position.set(140, 180, 500);
+ 
     const ambient = new THREE.AmbientLight(0x404040, 5);
     scene.add(ambient);
 
 
     const light = new THREE.DirectionalLight(0xffffff, 2);
-    light.position.set(10,10,-100);
+    light.position.set(200,0,0);
     scene.add(light);
     
     //Renderer
@@ -40,14 +40,16 @@ function init(){
     let loader = new THREE.GLTFLoader();
     loader.load('./../3d/scene.gltf', function(gltf){
         scene.add(gltf.scene);
-        crystal = gltf.scene.children[0];
+        tree = gltf.scene.children[0];
         animate();
     });
 }
 
 function animate(){
     requestAnimationFrame(animate);
-    crystal.rotation.z += 0.005;
+    tree.rotation.z += 0.002;
+    tree.rotation.x = 11.5;
+
     renderer.render(scene, camera);
 }
 
